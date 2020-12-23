3c8972ac94acff8a88a5403f585f55a699ee4b24
Asaf <asafgal28@gmail.com>
2020-12-23 00:53:22 -0500

    Uploaded MobileNet version and reorganized directories

---
+++ Slouch_Basic/pred.js
@@ -0,0 +1,155 @@
+CLASSES = {
+  0: 'slouch',
+  1: 'upright',
+};
+
+const MODEL_PATH =
+    'model.json';
+
+const IMAGE_SIZE = 192;
+const TOPK_PREDICTIONS = 2;
+
+let my_model;
+const demo = async () => {
+
+  status('Loading model...');
+
+  my_model = await tf.loadLayersModel(MODEL_PATH);
+
+  // Warmup the model. This isn't necessary, but makes the first prediction
+  // faster. Call `dispose` to release the WebGL memory allocated for the return
+  // value of `predict`.
+  my_model.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
+
+  status('');
+
+
+//  document.getElementById('file-container').style.display = '';
+};
+
+/**
+ * Given an image element, makes a prediction through my_model returning the
+ * probabilities of the top K classes.
+ */
+async function predict(imgElement) {
+
+  status('Predicting...');
+
+  // The first start time includes the time it takes to extract the image
+  // from the HTML and preprocess it, in additon to the predict() call.
+  const startTime1 = performance.now();
+  // The second start time excludes the extraction and preprocessing and
+  // includes only the predict() call.
+  let startTime2;
+  const logits = tf.tidy(() => {
+    // tf.browser.fromPixels() returns a Tensor from an image element.
+    const img = tf.browser.fromPixels(imgElement).toFloat();
+
+    // const offset = tf.scalar(127.5);
+    // Normalize the image from [0, 255] to [-1, 1].
+    // const normalized = img.sub(offset).div(offset);
+    const normalized = img.div(255.0);
+
+    // Reshape to a single-element batch so we can pass it to predict.
+    const batched = normalized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
+
+    startTime2 = performance.now();
+    // Make a prediction through my_model.
+    return my_model.predict(batched);
+  });
+
+  // Convert logits to probabilities and class names.
+  const classes = await getTopKClasses(logits, TOPK_PREDICTIONS);
+  const totalTime1 = performance.now() - startTime1;
+  const totalTime2 = performance.now() - startTime2;
+  status(`Done in ${Math.floor(totalTime1)} ms ` +
+      `(not including preprocessing: ${Math.floor(totalTime2)} ms)`);
+
+  // Show the classes in the DOM.
+  showResults(imgElement, classes);
+}
+
+/**
+ * Computes the probabilities of the topK classes given logits by computing
+ * softmax to get probabilities and then sorting the probabilities.
+ * @param logits Tensor representing the logits from my_model.
+ * @param topK The number of top predictions to show.
+ */
+async function getTopKClasses(logits, topK) {
+  const values = await logits.data();
+
+  const valuesAndIndices = [];
+  for (let i = 0; i < values.length; i++) {
+    valuesAndIndices.push({value: values[i], index: i});
+  }
+  valuesAndIndices.sort((a, b) => {
+    return b.value - a.value;
+  });
+  const topkValues = new Float32Array(topK);
+  const topkIndices = new Int32Array(topK);
+  for (let i = 0; i < topK; i++) {
+    topkValues[i] = valuesAndIndices[i].value;
+    topkIndices[i] = valuesAndIndices[i].index;
+  }
+
+  const topClassesAndProbs = [];
+  for (let i = 0; i < topkIndices.length; i++) {
+    topClassesAndProbs.push({
+      className: CLASSES[topkIndices[i]],
+      probability: topkValues[i]
+    })
+  }
+  return topClassesAndProbs;
+}
+
+//
+// UI
+//
+
+function showResults(imgElement, classes) {
+  const predictionContainer = document.createElement('div');
+  predictionContainer.className = 'pred-container';
+
+  const imgContainer = document.createElement('div');
+  imgContainer.appendChild(imgElement);
+  predictionContainer.appendChild(imgContainer);
+
+// Show the current highest predicted class
+  const slouching_status = document.getElementById('slouching_status');
+  highest_pred = classes[0].className;
+  if (highest_pred == 'slouch') {
+    slouching_status.innerHTML = "<p style='color:red;'>Slouching</p>";
+  } else if (highest_pred == 'upright') {
+    slouching_status.innerHTML = "<p style='color:blue;'>Upright</p>";
+  }
+
+  const probsContainer = document.createElement('div');
+  for (let i = 0; i < classes.length; i++) {
+    const row = document.createElement('div');
+    row.className = 'row';
+
+    const classElement = document.createElement('div');
+    classElement.className = 'cell';
+    classElement.innerText = classes[i].className;
+    row.appendChild(classElement);
+
+    const probsElement = document.createElement('div');
+    probsElement.className = 'cell';
+    probsElement.innerText = classes[i].probability.toFixed(3);
+    row.appendChild(probsElement);
+
+    probsContainer.appendChild(row);
+  }
+  predictionContainer.appendChild(probsContainer);
+
+  predictionsElement.insertBefore(
+      predictionContainer, predictionsElement.firstChild);
+}
+
+
+const demoStatusElement = document.getElementById('status');
+const status = msg => demoStatusElement.innerText = msg;
+
+const predictionsElement = document.getElementById('predictions');
+
+demo();
